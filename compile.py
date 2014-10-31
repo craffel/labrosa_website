6c25fe2c8fbc7d02c38334df1a265a8952c4fe19
Colin Raffel <craffel@gmail.com>
2014-10-31 23:07:58 +0800

    Pylint/PEP8

--- compile.py
+++ compile.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
-
-# <codecell>
+'''
+Compiles the LabROSA webpage.
+'''
 
 import lazyweb
 import os
@@ -9,36 +8,34 @@ import sys
 import urllib2
 import ujson as json
 
-# <codecell>
 
 def url_exists(url):
     '''
-    Check if a file exists on the web.  From http://stackoverflow.com/questions/2486145/python-check-if-url-to-jpg-exists
-    
+    Check if a file exists on the web.  From
+    http://stackoverflow.com/questions/2486145/python-check-if-url-to-jpg-exists
+
     Input:
         url - url to the web resource
     Output:
         result - True or False, depending on if the file exists
     '''
     try:
-        f = urllib2.urlopen(urllib2.Request(url))
+        urllib2.urlopen(urllib2.Request(url))
         return True
     except:
         return False
 
-# <codecell>
 
 def error(message):
     '''
     Print an error message and exit.
-    
+
     Input:
         message - error message to print
     '''
     print "ERROR: {}".format(message)
     sys.exit(-1)
 
-# <codecell>
 
 # Make sure the appropriate site directories exist
 for directory in ['people', 'projects', 'publications', 'contact']:
@@ -46,8 +43,6 @@ for directory in ['people', 'projects', 'publications', 'contact']:
         os.makedirs(os.path.join('site', directory))
     except:
         pass
-
-# <codecell>
 
 # People page
 print "Compiling people.tpl..."
@@ -59,7 +54,7 @@ statuses = ['Principal Investigator',
             'Post-Doctoral Research Assistant',
             'Undergraduate Research Assistant',
             'Visiting Researcher',
-            'PhD Graduate', 
+            'PhD Graduate',
             'Staff',
             'Other Researcher',
             'Friend',
@@ -72,19 +67,25 @@ with open('data/people.json', 'r') as f:
 for person in people['people']:
     # Check that their homepage exists
     if not url_exists(person['url']):
-        error("{}'s URL {} does not exist (should be an absolute url, e.g. http://google.com).".format(person['name'], person['url']))
+        error("{}'s URL {} does not exist (should be an absolute url, e.g."
+              " http://google.com).".format(person['name'], person['url']))
     # Check that the photo URL exists
     if not url_exists(person['photo']):
-        error("{}'s photo {} does not exist (should be an absolute url, e.g. http://google.com/me.jpg).".format(person['name'], person['photo']))
+        error("{}'s photo {} does not exist (should be an absolute url, e.g."
+              " http://google.com/me.jpg).".format(
+                  person['name'], person['photo']))
     # Check that the status is valid
     if person['status'] not in statuses:
-        error("{}'s status {} is not valid, should be one of {}".format(person['name'], person['status'], ", ".join(statuses)))
+        error("{}'s status {} is not valid, should be one of {}".format(
+            person['name'], person['status'], ", ".join(statuses)))
     # Check that their research description is not too long
     if len(person['research']) > 100:
-        error("{}'s research description is too long (longer than 100 characters)".format(person['name']))
+        error("{}'s research description is too long (longer than 100 "
+              "characters)".format(person['name']))
 
 # Sort people list by status, according to the order of the statuses list
-people['people'].sort(lambda a, b: cmp(statuses.index(a), statuses.index(b)), lambda x: x['status'])
+people['people'].sort(lambda a, b: cmp(statuses.index(a), statuses.index(b)),
+                      lambda x: x['status'])
 
 # TODO: Obfuscate all email addresses
 # for person in people['people']:
@@ -92,4 +93,3 @@ people['people'].sort(lambda a, b: cmp(statuses.index(a), statuses.index(b)), la
 
 # Write out the .html file
 lazyweb.compile('templates/people.tpl', people, 'site/people/index.html')
-
